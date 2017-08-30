2c83f512b06371ddb520ddadf83ffe71c6a753c1
Alison Hall <ahall7@sapient.com>
2017-08-30 15:55:48 -0400

    Working with React Events. Completed

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,28 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    /* One way of binding the function to the component: */
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You Changed the URL');
+        // first grab the text from the box
+        console.log(this.storeInput.value);
+        // second we're going to transition from / to /store/:storeId
+    }
+
     render() {
-        // Any where else
         return (
-            <form className="store-selector">
-                { /* Hello */ }
+            /* Second way of binding the function to the component: */
+            // <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+            /* Third way of binding the function to the component: */
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
                 <button type="submit">Visit Store &rarr;</button>
             </form>
         )
