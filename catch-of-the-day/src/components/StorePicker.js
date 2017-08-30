ac39217193ba681e9b94b96cecd2afb6549a182e
Alison Hall <ahall7@sapient.com>
2017-08-30 16:05:28 -0400

    All About React Router. Completed

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -12,8 +12,10 @@ class StorePicker extends React.Component {
         event.preventDefault();
         console.log('You Changed the URL');
         // first grab the text from the box
-        console.log(this.storeInput.value);
+        const storeId = this.storeInput.value;
+        console.log(`Going to ${storeId}`);
         // second we're going to transition from / to /store/:storeId
+        this.context.router.transitionTo(`/store/${storeId}`);
     }
 
     render() {
@@ -28,6 +30,10 @@ class StorePicker extends React.Component {
             </form>
         )
     }
+}
+
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
 }
 
 export default StorePicker;
