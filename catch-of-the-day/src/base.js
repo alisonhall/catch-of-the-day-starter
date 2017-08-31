e1b192884daf0708b41377ffffcf5fbb89804cda
Alison Hall <ahall7@sapient.com>
2017-08-31 14:17:47 -0400

    Persisting our State with Firebase. Completed

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyB0DOFtdSYTudtPQ0f93KKCLJ9-4qWqRQQ",
+    authDomain: "catch-of-the-day-alison-hall.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-alison-hall.firebaseio.com",
+});
+
+export default base;
